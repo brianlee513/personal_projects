2dac79f1ba131e72db680a8b9b579a7b962a5de5
Brian Lee <kjl3@illinois.edu>
2021-02-17 15:37:39 +0900

    graphing tutorial

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import matplotlib.pyplot as pyplot
 import pickle
 from matplotlib import style
 
+
 data = pd.read_csv('student-mat.csv', sep=";")
 
 data = data[["G1", "G2", "G3", "studytime", "failures", "absences"]]
@@ -15,17 +16,21 @@ predict = "G3"
 
 X = np.array(data.drop([predict], 1))
 Y = np.array(data[predict])
+x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(X, Y, test_size = 0.1)
 
-x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(X, Y, test_size=0.1)
+best = 0
+for x in range(30):
+    x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(X, Y, test_size = 0.1)
+    linear = linear_model.LinearRegression()
 
-linear = linear_model.LinearRegression()
+    linear.fit(x_train, y_train)
+    accuracy = linear.score(x_test, y_test)
+    print(accuracy)
 
-linear.fit(x_train, y_train)
-accuracy = linear.score(x_test, y_test)
-print(accuracy)
-
-with open("studentmodel.pickle", "wb") as f:
-    pickle.dump(linear, f)
+    if accuracy > best:
+        best = accuracy
+        with open("studentmodel.pickle", "wb") as f:
+            pickle.dump(linear, f)
 
 pickle_in = open("studentmodel.pickle", "rb")
 linear = pickle.load(pickle_in)
@@ -37,3 +42,10 @@ prediction = linear.predict(x_test)
 
 for x in range(len(prediction)):
     print(prediction[x], x_test[x], y_test[x])
+
+p = "G1"
+style.use("ggplot")
+pyplot.scatter(data[p], data["G3"])
+pyplot.xlabel(p)
+pyplot.ylabel("Final Score")
+pyplot.show()
