9a073c0f5b759507d94665e32c1b5c039fd9a862
Brian Lee <kjl3@illinois.edu>
2022-02-25 23:46:56 -0600

    auto_mpg-linear regression

--- Pose Estimation/pose_estimation.py
+++ Pose Estimation/pose_estimation.py
@@ -1,5 +1,9 @@
+
+import cv2
 import pafy
-from cv2 import cv2 
+
+
+
 
 alma_cam_url = "https://youtu.be/h6hzVOwaN_4"
 alma_video = pafy.new(alma_cam_url)
@@ -7,11 +11,21 @@ alma_video = pafy.new(alma_cam_url)
 quad_cam_url = "https://youtu.be/Yq4WLVuX0bU"
 quad_video = pafy.new(quad_cam_url)
 
-best_alma = alma_video.getbest(prefype = 'mp4')
-best_quad = quad_video.getbest(prefype = 'mp4')
+best_alma = alma_video.getbest()
+best_quad = quad_video.getbest()
 
-capture_alma = cv2.VideoCapture(best_alma)
-capture_quad = cv2.VideoCapture(best_alma)
+capture_alma = cv2.VideoCapture(best_alma.url)
+capture_quad = cv2.VideoCapture(best_quad.url)
 
-while True:
-    grabbed, frame = capture_alma.read()
+
+while cv2.waitKey(33) < 0:
+    quad_ret, quad_frame = capture_quad.read()
+    resize_quad = cv2.resize(quad_frame, (1920, 1080))
+    alma_ret, alma_frame = capture_alma.read()
+    resize_alma = cv2.resize(alma_frame, (1920, 1080))
+
+    cv2.imshow("VideoFrame", resize_alma)
+    cv2.imshow("Quad", resize_quad)
+
+capture_alma.release()
+cv2.destroyAllWindows()
