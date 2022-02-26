cd6e0457e6375f8967664698526f37f526a09868
Brian Lee <kjl3@illinois.edu>
2022-02-26 00:54:56 -0600

    resize according to display

--- Pose Estimation/pose_estimation.py
+++ Pose Estimation/pose_estimation.py
@@ -2,9 +2,6 @@
 import cv2
 import pafy
 
-
-
-
 alma_cam_url = "https://youtu.be/h6hzVOwaN_4"
 alma_video = pafy.new(alma_cam_url)
 
@@ -19,13 +16,19 @@ capture_quad = cv2.VideoCapture(best_quad.url)
 
 
 while cv2.waitKey(33) < 0:
+    cv2.namedWindow("VideoFrame", cv2.WINDOW_NORMAL)
+    cv2.namedWindow("Quad", cv2.WINDOW_NORMAL)
+    cv2.resizeWindow("VideoFrame", 1920, 1080)
+    cv2.resizeWindow("Quad", 1920, 1080)
     quad_ret, quad_frame = capture_quad.read()
-    resize_quad = cv2.resize(quad_frame, (1920, 1080))
-    alma_ret, alma_frame = capture_alma.read()
-    resize_alma = cv2.resize(alma_frame, (1920, 1080))
 
-    cv2.imshow("VideoFrame", resize_alma)
-    cv2.imshow("Quad", resize_quad)
+    #cv2.resizeWindow("VideoFrame", (1920, 1080))
+    alma_ret, alma_frame = capture_alma.read()
+    #cv2.resizeWindow(alma_frame, 1920, 1080)
+
+    cv2.imshow("VideoFrame", alma_frame)
+
+    cv2.imshow("Quad", quad_frame)
 
 capture_alma.release()
 cv2.destroyAllWindows()
